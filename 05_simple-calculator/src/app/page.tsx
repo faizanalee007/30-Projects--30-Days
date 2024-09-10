ac690beaa2aafcd58a441b63cf71923d3b245f74
Faizan Alee <160243661+faizanalee007@users.noreply.github.com>
2024-09-11 02:40:32 +0500

    Skin Choosing Feature Added

--- 05_simple-calculator/src/app/page.tsx
+++ 05_simple-calculator/src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { ChangeEvent, useState ,useEffect} from "react";
+import { ChangeEvent, useState, useEffect } from "react";
 
 
 export default function Home() {
@@ -9,7 +9,7 @@ export default function Home() {
   const [Number1, setNumber1] = useState("")
   const [Number2, setNumber2] = useState("")
   const [clearBtn, setClearBtn] = useState(false)
-  const [loading, setLoading] = useState(true); 
+  const [loading, setLoading] = useState(true);
 
 
   const error = 'Number to daaliyee!'
@@ -19,9 +19,8 @@ export default function Home() {
   const Number1Change = (e: ChangeEvent<HTMLInputElement>) => {
 
     setNumber1(e.target.value)
-    
-  }
 
+  }
 
   // Change state of Number2 input Field
   const Number2Change = (e: ChangeEvent<HTMLInputElement>) => {
@@ -29,14 +28,12 @@ export default function Home() {
     setNumber2(e.target.value)
   }
 
-
   //Additon
   const addButton = () => {
 
     Number1 && Number2 ? setAnswer((parseFloat(Number1) + parseFloat(Number2)).toString()) : setAnswer(error)
     setClearBtn(true)
   }
-
 
   //Subtraction
   const subtractButton = () => {
@@ -68,8 +65,16 @@ export default function Home() {
 
   }
 
+  // Calculator Interface Color changing
 
-  // const [bgColor, setBgColor] = useState()
+  const [bgColor, setBgColor] = useState(`bg-gray-600`)
+
+  const bgChange = (color:string)=> {
+
+    setBgColor(color)
+  
+
+  }
 
 
   useEffect(() => {
@@ -91,31 +96,55 @@ export default function Home() {
   return (
     <div className="bg-[url('/bg.png')] bg-cover bg-center bg-no-repeat min-h-screen w-full flex items-center justify-center">
 
-      <div className="bg-gray-500  shadow-lg shadow-black hover:scale-110 hover:shadow-black hover:shadow-2xl transition-all duration-300 border-4 p-8 border-black rounded-3xl  flex flex-col items-center justify-center gap-6" >
+      <div className=" flex items-center justify-center">
 
-        <h1 className="text-3xl text-black text-left font-bold">Simple Calculator</h1>
+        <div className=" flex flex-col items-center justify-center mr-12 space-y-7 p-4  bg-gray-500 bg-opacity-50 border-2 rounded-2xl border-gray-800 shadow-lg shadow-black hover:-translate-x-5 transition-all duration-300">
+          <h2 className="font-bold text-xl -mb-4">SKINS</h2>
+          
+          <button onClick={()=>bgChange('bg-cyan-500')} className="w-12 h-12 rounded-full border border-gray-100 hover:scale-105 transition-all duration-200 bg-cyan-500"></button>
 
-        <p className="text-gray-200 text-2xl text-right font-bold font-mono -mt-4">By Faizan Ahmed</p>
+          <button onClick={()=>bgChange('bg-[#d14d52]')} className="w-12 h-12 rounded-full border border-gray-100 hover:scale-105 transition-all duration-200 bg-[#d14d52]"></button>
 
-        <div className="mt-4 gap-3 flex ">
-          <input className="inputField rounded-md w-40" type="number" placeholder="number 1" value={Number1} onChange={Number1Change} />
+          <button onClick={()=>bgChange('bg-blue-500')} className="w-12 h-12 rounded-full border border-gray-100 hover:scale-105 transition-all duration-200 bg-blue-400"></button>
 
-          <input className="inputField rounded-md w-40" type="number" placeholder="number 2" value={Number2} onChange={Number2Change} />
+          <button onClick={()=>bgChange('bg-[#8f8aca]')} className="w-12 h-12 rounded-full border border-gray-100 hover:scale-105 transition-all duration-200 bg-[#8f8aca]"></button>
+
+          <button onClick={()=>bgChange('bg-[#e0e05a]')} className="w-12 h-12 rounded-full border border-gray-100 hover:scale-105 transition-all duration-200 bg-[#e0e05a]"></button>
         </div>
 
-        <div className=" flex justify-center items-center gap-3">
-          <button className="px-8 py-2 bg-gray-800 hover:bg-gray-700 rounded-md text-gray-100 hover:scale-105 transition-all text-xl font-bold duration-300" onClick={addButton}>+</button>
-          <button className="px-8 py-2 bg-gray-800 hover:bg-gray-700 rounded-md text-gray-100 hover:scale-105 transition-all text-xl font-bold duration-300" onClick={subtractButton}>-</button>
-          <button className="px-8 py-2 bg-gray-800 hover:bg-gray-700 rounded-md text-gray-100 hover:scale-105 transition-all text-xl font-bold duration-300" onClick={multiplyButton}>*</button>
-          <button className="px-8 py-2 bg-gray-800 hover:bg-gray-700 rounded-md text-gray-100 hover:scale-105 transition-all text-xl font-bold duration-300" onClick={devideButton}>/</button>
-        </div>
+        <div className={`${bgColor}  shadow-lg shadow-black hover:scale-110 hover:shadow-black hover:shadow-2xl transition-all duration-300 border-4 p-8 border-black rounded-3xl  flex flex-col items-center justify-center gap-6`} >
 
-        <input className="bg-gray-100 inputField font-bold outline outline-gray-500 w-[300px] rounded-md mt-4 text-2xl" type="number" placeholder="result" value={answer} readOnly />
+          <h1 className="text-3xl text-black text-left font-bold">Simple Calculator</h1>
 
-        { clearBtn && <button className=" px-8 py-2 bg-gray-800 hover:bg-gray-700 rounded-md text-gray-100 hover:scale-105 transition-all text-xl duration-300" onClick={clearButton}>Clear</button>}
+          <p className="text-gray-700 text-2xl text-right font-extrab500 font-mono -mt-4">By Faizan Ahmed</p>
+
+          <div className="mt-4 gap-3 flex ">
+            <input className="inputField rounded-md w-40" type="number" placeholder="number 1" value={Number1} onChange={Number1Change} />
+
+            <input className="inputField rounded-md w-40" type="number" placeholder="number 2" value={Number2} onChange={Number2Change} />
+          </div>
+
+          <div className=" flex justify-center items-center gap-3">
+            <button className="px-8 py-2 bg-gray-800 hover:bg-gray-700 rounded-md text-gray-100 hover:scale-105 transition-all text-xl font-bold duration-300" onClick={addButton}>+</button>
+            <button className="px-8 py-2 bg-gray-800 hover:bg-gray-700 rounded-md text-gray-100 hover:scale-105 transition-all text-xl font-bold duration-300" onClick={subtractButton}>-</button>
+            <button className="px-8 py-2 bg-gray-800 hover:bg-gray-700 rounded-md text-gray-100 hover:scale-105 transition-all text-xl font-bold duration-300" onClick={multiplyButton}>*</button>
+            <button className="px-8 py-2 bg-gray-800 hover:bg-gray-700 rounded-md text-gray-100 hover:scale-105 transition-all text-xl font-bold duration-300" onClick={devideButton}>/</button>
+          </div>
+
+          <input className="bg-gray-100 inputField font-bold outline outline-gray-500 w-[300px] rounded-md mt-4 text-2xl" type="number" placeholder="result" value={answer} readOnly />
+
+          {clearBtn && <button className=" px-8 py-2 bg-gray-800 hover:bg-gray-700 rounded-md text-gray-100 hover:scale-105 transition-all text-xl duration-300" onClick={clearButton}>Clear</button>}
 
         
+
+
+
+
+        </div>
+
+
       </div>
+
 
 
 
